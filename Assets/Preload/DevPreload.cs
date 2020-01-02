2325b34297ea8eff9c2a90a53a2343419904dd7b
Isaac - PC <IsaacTFagg@gmail.com>
2020-01-02 16:45:43 +0000

    Hand Models implemented to gamescene

---
+++ Assets/Preload/DevPreload.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DevPreload : MonoBehaviour
+{
+	void Awake()
+	{
+		GameObject check = GameObject.Find("_app");
+		if (check == null)
+		{
+			UnityEngine.SceneManagement.SceneManager.LoadScene("_preload", LoadSceneMode.Additive);
+		}
+	}
+}
